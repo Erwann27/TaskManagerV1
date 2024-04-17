c2861279ded521bcd512801e5d8f9875ac20d6bb
Erwann27 <montagne.erwann.2@gmail.com>
2024-04-17 10:37:02 +0200

    feature : started to create view

---
+++ src/main/java/View/TaskManagerApplication.java
@@ -0,0 +1,11 @@
+package View;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TaskManagerApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
